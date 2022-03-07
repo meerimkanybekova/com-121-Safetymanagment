e5ce558b2a51dd5f8a5c3937349e15ea84e8a008
meerimkanybekova <kanybekova_m@auca.kg>
2022-03-07 07:07:10 +0100

    Finish problem-02 from lab-01

---
+++ lab-01/problem-02/02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "utilities.h"
+
+int main() {
+	print_decor_line();
+	puts("|hello, world|");
+	print_decor_line();
+	return 0;
+}
---
+++ lab-01/problem-02/utilities.c
@@ -0,0 +1,7 @@
+#include "utilities.h"
+
+#include <stdio.h>
+
+void print_decor_line() {
+	puts("**************");
+}
